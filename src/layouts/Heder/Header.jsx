ed6cc2e9f1ae9424b473d975b0114f8d242a7446
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2025-09-22 13:40:29 +0300

    ui header and js burger menu for tablet->mobile

--- src/layouts/Heder/Header.jsx
+++ src/layouts/Heder/Header.jsx
@@ -2,6 +2,7 @@ import './Header.scss'
 import Logo from "@/components/Logo/Logo.jsx";
 import classNames from "classnames";
 import Button from "@/components/Button/index.js";
+import BurgerButton from "@/components/BurgerButton/index.js";
 
 export default (props) => {
     // url для использования isActive
@@ -27,39 +28,50 @@ export default (props) => {
     ]
 
     return (
-        <header className="header">
+        <header className="header" data-js-overlay-menu="">
             <div className="header__inner container">
                 <Logo className='header__logo' loading='eager'/>
-                <nav className="header__menu">
-                    <ul className="header__menu-list">
-                        {
-                            menuItems.map(({label, href}, index) => (
-                                <li key={index} className="header__menu-item">
-                                    <a href={href} className={classNames("header__menu-link", {
-                                        'is-active': href === url
-                                    })}>{label}</a>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                </nav>
-                <div className="header__actions">
-                    <Button
-                        label="Search"
-                        isLabelHidden
-                        mode='transparent'
-                        iconName="search"
-                        className='header__button'
-                    />
-                    <Button
-                        label="Notificaations"
-                        isLabelHidden
-                        mode='transparent'
-                        iconName="notification"
-                        className='header__button'
+                <dialog
+                    className="header__overlay-menu-dialog"
+                    data-js-overlay-menu-dialog=""
+                >
+                    <nav className="header__menu">
+                        <ul className="header__menu-list">
+                            {
+                                menuItems.map(({label, href}, index) => (
+                                    <li key={index} className="header__menu-item">
+                                        <a href={href} className={classNames("header__menu-link", {
+                                            'is-active': href === url
+                                        })}>{label}</a>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </nav>
+                    <div className="header__actions">
+                        <Button
+                            label="Search"
+                            isLabelHidden
+                            mode='transparent'
+                            iconName="search"
+                            className='header__button'
+                        />
+                        <Button
+                            label="Notificaations"
+                            isLabelHidden
+                            mode='transparent'
+                            iconName="notification"
+                            className='header__button'
 
-                    />
-                </div>
+                        />
+                    </div>
+                </dialog>
+                <BurgerButton
+                    className="header__burger-button visible-tablet"
+                    extraAttrs={{
+                        'data-js-overlay-menu-burger-button': ''
+                    }}
+                />
             </div>
         </header>
     )
---
+++ src/modules/OverlayMenu.js
@@ -0,0 +1,38 @@
+class OverlayMenu {
+    // css селекторы к нужным элементам разметки
+    selectors = {
+        root: '[data-js-overlay-menu]',
+        dialog: '[data-js-overlay-menu-dialog]',
+        burgerButton: '[data-js-overlay-menu-burger-button]',
+    }
+    // css состояние isActive IsLock
+    stateClasses = {
+        isActive: 'is-active',
+        isLock: 'is-lock',
+    }
+    // выполняется первым при инициализации класса
+    // находим элементы в DOM
+    constructor() {
+        this.rootElement = document.querySelector(this.selectors.root);
+        this.dialogElement = this.rootElement.querySelector(this.selectors.dialog);
+        this.burgerButtonElement = this.rootElement.querySelector(this.selectors.burgerButton);
+        this.bindEvents();
+    }
+
+    onBurgerButtonClick = () => {
+        // добавляем и удаляем активный класс при нажатии
+        this.burgerButtonElement.classList.toggle(this.stateClasses.isActive);
+        // меняем open for dialog // для открытия и закрытия при клике
+        this.dialogElement.open = !this.dialogElement.open;
+        document.documentElement.classList.toggle(this.stateClasses.isLock);
+
+    }
+
+    // привязываем слушатели к DOM лементам
+    bindEvents() {
+        this.burgerButtonElement.addEventListener('click', this.onBurgerButtonClick)
+    }
+
+}
+
+export default OverlayMenu;
