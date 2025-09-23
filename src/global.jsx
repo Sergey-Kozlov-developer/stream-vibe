af4652a16a22d909b005614914a882f9eaf5877d
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2025-09-23 16:54:24 +0300

    github-pages

--- src/global.jsx
+++ src/global.jsx
@@ -1,27 +1,39 @@
-import '@/styles'
-import {Head} from "minista"
+import "@/styles";
+import { Head } from "minista";
 import Header from "@/layouts/Heder/Header";
 import Content from "@/layouts/Content/Content";
 import Footer from "@/layouts/Footer/Footer";
 
 export default function (props) {
-    // url для использования isActive
-    const {children, title, url} = props
-    return (
-        <>
-            <Head htmlAttributes={{lang: 'en'}}>
-                <title>Stream Vibe | {title}</title>
-                <script src='/src/main.js' type="module"/>
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-                <link rel="manifest" href="/site.webmanifest"/>
-            </Head>
-            <Header url={url}/>
-            <Content>
-                {children}
-            </Content>
-            <Footer/>
-        </>
-    )
+	// url для использования isActive
+	const { children, title, url } = props;
+	return (
+		<>
+			<Head htmlAttributes={{ lang: "en" }}>
+				<title>Stream Vibe | {title}</title>
+				<script src="/src/main.js" type="module" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
+			</Head>
+			<Header url={url} />
+			<Content>{children}</Content>
+			<Footer />
+		</>
+	);
 }
